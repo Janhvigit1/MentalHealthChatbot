aabfc073f2cf685f835c97c60f49d395ba4addc6
Janhvigit1 <factzit444@gmail.com>
2026-02-07 18:38:46 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="mental-chatbot",
+    version="0.1.0",
+    author="Janhvi",
+    author_email="singhthakurjanhvi046@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
